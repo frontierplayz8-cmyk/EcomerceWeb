de1631aeb15354dc53fc1a3aedc389ab2fc59d75
Talha <frontierplayz8@gmail.com>
2025-11-29 15:05:45 +0500

    Updates

--- assets/productpage-B2H3-sr8.js
+++ assets/productpage-B2H3-sr8.js
@@ -1,4 +1,4 @@
-import{d as r,r as h,u as k,e as w,j as e}from"./index-BbSehDXj.js";import{N as x,F as b,S}from"./footer-DRLTGRPr.js";import{i as n,a as o,b as c,c as l,d}from"./image5-CJ9BDC9X.js";import{i as u}from"./image6-AEIrMTuM.js";import{L as C,y as P}from"./ReactToastify-D3QWtMIK.js";/**
+import{d as r,r as h,u as k,e as w,j as e}from"./index-DX3RQukl.js";import{N as x,F as b,S}from"./footer-CLfO69vm.js";import{i as n,a as o,b as c,c as l,d}from"./image5-CJ9BDC9X.js";import{i as u}from"./image6-AEIrMTuM.js";import{L as C,y as P}from"./ReactToastify-yScAZwGN.js";/**
  * @license lucide-react v0.554.0 - ISC
  *
  * This source code is licensed under the ISC license.
